49fcc8ac840976ff9dab50ccca2fe6837134823d
Tobias Effner <tobias.effner@valtech-mobility.com>
2021-12-15 09:02:49 +0100

    Delete almost everything for now

--- main.tf
+++ main.tf
@@ -39,84 +39,84 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-data "digitalocean_project" "tmc" {
-  name = "tmc"
-}
-
-resource "digitalocean_project_resources" "tmc" {
-  project   = data.digitalocean_project.tmc.id
-  resources = concat(module.dns.domain_urns)
-}
-
-module "cluster" {
-  source = "./cluster"
-
-  project = var.project
-  region  = var.do_region
-
-  cluster_version_prefix = var.cluster_version_prefix
-  default_node_size      = var.default_node_size
-  min_nodes              = var.min_nodes
-  max_nodes              = var.max_nodes
-}
-
-module "dns" {
-  source = "./dns"
-
-  project = var.project
-
-  domains = var.domains
-}
-
-module "balancer" {
-  source = "./balancer"
-
-  project = var.project
-  region  = var.do_region
-}
-
-module "flux" {
-  source = "./flux"
-
-  project = var.project
-
-  flux_config_repo  = var.flux_config_repo
-  flux_git_branch   = var.flux_git_branch
-  flux_git_password = var.flux_git_password
-  flux_git_username = var.flux_git_username
-}
-
-module "cert-manager" {
-  source = "./cert_manager"
-
-  project = var.project
-
-  # digitalocean_token = var.digitalocean_token
-  contact_mail = var.contact_mail
-  domains      = [] # var.domains
-
-  depends_on = [module.cluster]
-}
-
-module "ingress_nginx" {
-  source = "./ingress_nginx"
-
-  project = var.project
-
-  balancer_id = module.balancer.balancer_id
-
-  depends_on = [module.cluster]
-}
-
-module "external_dns" {
-  source = "./external_dns"
-
-  project = var.project
-
-  digitalocean_token = var.digitalocean_token
-
-  depends_on = [module.cluster]
-}
+//data "digitalocean_project" "tmc" {
+//  name = "tmc"
+//}
+//
+//resource "digitalocean_project_resources" "tmc" {
+//  project   = data.digitalocean_project.tmc.id
+//  resources = concat(module.dns.domain_urns)
+//}
+//
+//module "cluster" {
+//  source = "./cluster"
+//
+//  project = var.project
+//  region  = var.do_region
+//
+//  cluster_version_prefix = var.cluster_version_prefix
+//  default_node_size      = var.default_node_size
+//  min_nodes              = var.min_nodes
+//  max_nodes              = var.max_nodes
+//}
+//
+//module "dns" {
+//  source = "./dns"
+//
+//  project = var.project
+//
+//  domains = var.domains
+//}
+//
+//module "balancer" {
+//  source = "./balancer"
+//
+//  project = var.project
+//  region  = var.do_region
+//}
+//
+//module "flux" {
+//  source = "./flux"
+//
+//  project = var.project
+//
+//  flux_config_repo  = var.flux_config_repo
+//  flux_git_branch   = var.flux_git_branch
+//  flux_git_password = var.flux_git_password
+//  flux_git_username = var.flux_git_username
+//}
+//
+//module "cert-manager" {
+//  source = "./cert_manager"
+//
+//  project = var.project
+//
+//  # digitalocean_token = var.digitalocean_token
+//  contact_mail = var.contact_mail
+//  domains      = [] # var.domains
+//
+//  depends_on = [module.cluster]
+//}
+//
+//module "ingress_nginx" {
+//  source = "./ingress_nginx"
+//
+//  project = var.project
+//
+//  balancer_id = module.balancer.balancer_id
+//
+//  depends_on = [module.cluster]
+//}
+//
+//module "external_dns" {
+//  source = "./external_dns"
+//
+//  project = var.project
+//
+//  digitalocean_token = var.digitalocean_token
+//
+//  depends_on = [module.cluster]
+//}
 
 module "s3_data" {
   source = "./s3"
